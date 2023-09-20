22c1532719ddef0f8ac41e61c197d98739d1bf71
marsonshine <ms27946@163.com>
2023-09-20 14:18:38 +0800

    init: my websocket and benchmark

---
+++ WebSocketExample/Domain/RedisSessionManager.cs
@@ -0,0 +1,18 @@
+﻿namespace WebSocketExample.Domain
+{
+    public class RedisSessionManager
+    {
+        internal readonly struct WSConnectSession
+        {
+            public string ClientId1 { get; init; }
+            public string ClientId2 { get; init; }
+            public Guid SessionId { get; init; }
+        }
+
+        internal readonly struct ClientRequestPackage
+        {
+            public string ClientId { get; init; }
+            public List<object> Contents { get; init; }
+        }
+    }
+}
---
+++ WebSocketExample/Domain/WSConnectionManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+using System.Net.WebSockets;
+
+namespace WebSocketExample.Domain
+{
+    public class WSConnectionManager
+    {
+        private readonly ConcurrentDictionary<string, WebSocket> _sockets = new();
+
+        public WebSocket GetSocketByClientId(string clientId) => _sockets[clientId];
+
+        public void AddSocket(string clientId, WebSocket webSocket) => _sockets[clientId] = webSocket;
+
+        public void RemoveSocket(string clientId) => _sockets.Remove(clientId, out _);
+    }
+}
